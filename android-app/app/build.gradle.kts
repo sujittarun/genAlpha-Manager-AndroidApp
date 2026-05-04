354f4a2b6f9d5cf09048a875e7836a9d6fcf59a3
sujittarun <tarun.sujit@gmail.com>
2026-05-04 19:10:57 +0530

    Prepare Android test build for sharing

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     applicationId = "com.genalpha.cricketacademy"
     minSdk = 24
     targetSdk = 35
-    versionCode = 41
-    versionName = "1.0.41"
+    versionCode = 42
+    versionName = "1.0.42"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
